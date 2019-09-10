e33308e2153e0eb2c63a14bdbc2b0afec6bc5e8c
SeiichiN <billie175@gmail.com>
2019-09-10 15:16:16 +0900

    pipe 処理

---
+++ static_pipe.js
@@ -0,0 +1,22 @@
+// static.js
+// p97
+// 静的ファイルサーバー
+
+var http = require('http');
+var parse = require('url').parse;
+var join = require('path').join;
+var fs = require('fs');
+
+var root = __dirname;
+
+var server = http.createServer( function (req, res) {
+  var url = parse( req.url );
+  var path = join( root, url.pathname );
+  var stream = fs.createReadStream( path );
+  stream.pipe(res);
+});
+
+server.listen(3000);
+
+console.log('Server listening in port 3000');
+
